bb2ff87760014a590ac7da288af01542b59724ea
Paulo Soares <psoares.bj@gmail.com>
2018-12-23 11:43:13 +0000

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import generateProps from './generator'
+import mq from './mq'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export {
+  generateProps,
+  mq
 }
