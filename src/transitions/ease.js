8f0680d6eb0ea99db286ecbb8ef5bb187937e221
Paulo Soares <psoares.bj@gmail.com>
2019-01-19 10:30:11 +0000

    Fix #7

--- src/transitions/ease.js
+++ src/transitions/ease.js
@@ -1,5 +1,5 @@
 const ease = {
-  inSine: 'cubic-bezier(0.47, 0, 0.745, 0.715',
+  inSine: 'cubic-bezier(0.47, 0, 0.745, 0.715)',
   outSine: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
   inOutSine: 'cubic-bezier(0.445, 0.05, 0.55, 0.95)',
   inQuad: 'cubic-bezier(0.55, 0.085, 0.68, 0.53)',
